fd74c9330dc7d05d0c02289901f4b7b54fdde2e2
LucaAngioloni <lucaangioloni@gmail.com>
2018-05-11 12:28:49 +0200

    make file test

---
+++ IntegralImages/CUDA/cudacode.h
@@ -0,0 +1,2 @@
+
+void image_integral(int *a, int*b, int rowsTotal,int colsTotal);
--- IntegralImages/CUDA/main.cpp
+++ IntegralImages/CUDA/main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
 #include <cuda_runtime.h>
+#include "cudacode.h"
 
 using namespace std;
-
-__global__ void image_integral(int *a, int*b, int rowsTotal,int colsTotal)
-{
-    // Thread Ids equal to block Ids because the each blocks contains one thread only.
-    int col = blockIdx.x;
-    int row = blockIdx.y;
-    int temp=0;
-
-    if(col < colsTotal && row < rowsTotal)
-    {
-        // The first loop iterates from zero to the Y index of the thread which represents the corresponding element of the output/input array.  
-        for(int r=0;r<=row;r++)
-        {
-            // The second loop iterates from zero to the X index of the thread which represents the corresponding element of the output/input array  
-            for(int c=0; c<=col; c++)
-            {
-                temp = temp+a[r*colsTotal+c];
-            }
-        }
-    }
-
-    //Transfer the final result to the output array
-    b[row*colsTotal+col]=temp;
-}
 
 void main()
 {
